d4e3eedee979d00c1c0918c78344c9f462f17040
Mateus Melchiades <matbme@duck.com>
2023-02-17 20:05:01 -0300

    Finalize UI

--- vanilla_installer/defaults/encryption.py
+++ vanilla_installer/defaults/encryption.py
@@ -26,6 +26,8 @@ class VanillaDefaultEncryption(Adw.Bin):
     password_entry = Gtk.Template.Child()
     confirm_password_entry = Gtk.Template.Child()
 
+    valid_passwords = False
+
     def __init__(self, window, distro_info, key, step, **kwargs):
         super().__init__(**kwargs)
         self.__window = window
@@ -33,16 +35,36 @@ class VanillaDefaultEncryption(Adw.Bin):
         self.__key = key
         self.__step = step
 
+        self.use_encryption_switch.connect("state-set", self.__on_encryption_switch_toggled)
         self.btn_next.connect("clicked", self.__on_btn_next_clicked)
         self.password_entry.connect('changed', self.__on_password_changed)
         self.confirm_password_entry.connect('changed', self.__on_password_changed)
 
+        self.__verify_continue()
+
     def __on_btn_next_clicked(self, button):
         self.__window.next()
 
-    # def __on_password_changed(self, *args):
-    #     password = self.password_entry.get_text().strip()
-    #     confirm_password = self.confirm_password_entry.get_text().strip()
-    #
-    #     if password and confirm_password and password == confirm_password:
-    #
+    def __on_encryption_switch_toggled(self, widget, state):
+        if widget.get_active():
+            self.__on_password_changed()
+        else:
+            self.__verify_continue()
+
+    def __on_password_changed(self, *args):
+        password = self.password_entry.get_text().strip()
+        confirm_password = self.confirm_password_entry.get_text().strip()
+
+        if password == "" and confirm_password == "":
+            self.valid_passwords = False;
+        elif password == confirm_password:
+            self.valid_passwords = True;
+            self.confirm_password_entry.remove_css_class('error')
+        else:
+            self.valid_passwords = False;
+            self.confirm_password_entry.add_css_class('error')
+
+        self.__verify_continue();
+
+    def __verify_continue(self):
+        self.btn_next.set_sensitive(not self.use_encryption_switch.get_active() or self.valid_passwords)
